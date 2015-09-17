b086576654eff051c9e75736d3376db9c9512e76
AZ technology <aztec@localhost.domain>
2015-09-17 18:24:22 +0800

    Add Android.mk
    Change-Id: I8f988816965c2f4048e0fde08baeb28c24eeea78

---
+++ Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := nodejs
+LOCAL_MODULE_CLASS := DATA
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+$(LOCAL_INSTALLED_MODULE): $(LOCAL_BUILT_MODULE)
+
+.PHONY: $(LOCAL_BUILT_MODULE)
+$(LOCAL_BUILT_MODULE):
+	(cd node;  \
+	export TOOLCHAIN=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/host/x86_64-linux-glibc2.11-4.8/bin; \
+	export AR=$$TOOLCHAIN/x86_64-linux-ar; \
+	export CC=$$TOOLCHAIN/x86_64-linux-gcc; \
+	export CXX=$$TOOLCHAIN/x86_64-linux-g++; \
+	export LINK=$$TOOLCHAIN/x86_64-linux-g++; \
+	./configure --prefix=$(ANDROID_BUILD_TOP)/$(HOST_OUT); \
+	$(MAKE) && $(MAKE) install )
+	(cd node; \
+	export TOOLCHAIN=$(ANDROID_BUILD_TOP)/toolchain/linux-x86/gcc-linaro-arm-linux-gnueabihf-4.8-2014.04_linux/bin; \
+	export AR=$$TOOLCHAIN/arm-linux-gnueabihf-ar; \
+	export CC=$$TOOLCHAIN/arm-linux-gnueabihf-gcc; \
+	export CXX=$$TOOLCHAIN/arm-linux-gnueabihf-g++; \
+	export LINK=$$TOOLCHAIN/arm-linux-gnueabihf-g++; \
+	./configure \
+	  --fully-static \
+	  --dest-cpu=arm \
+	  --dest-os=linux && \
+	$(MAKE) ) && mkdir -p $(@D) && cp node/out/Release/iojs $@
